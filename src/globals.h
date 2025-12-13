2d8628a204f2abc8610d4248f4e87c50ebfbaf98
Gopal Jain <jaingopal1980.24@kgpian.iitkgp.ac.in>
2025-12-13 17:01:08 +0530

    rearranged

--- src/globals.h
+++ src/globals.h
@@ -8,6 +8,19 @@
 using namespace std;
 
 namespace fs = std::filesystem;
-string cwd;
-vector<fs::path> exec_folders;
-vector<string>builtin;
+
+#ifndef cwd
+    #define cwd 
+    string cwd;
+#endif
+
+#ifndef exec_folders
+    #define exec_folders
+    vector<fs::path> exec_folders;
+#endif
+
+#ifndef builtin
+    #define builtin
+    vector<string>builtin;
+#endif
+    
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "commands.h"
 #include "functions.h"
+#include "globals.h"
 cwd="";
 builtin={"type","exit","echo","pwd"};
 
